0fbb083c4bbcacb6cc7d6fdcd96fca583890cee1
dao manh <daoducmanh28101997@gmail.com>
2018-11-02 16:52:15 +0700

    fisrt commit

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -43,7 +43,50 @@ $( document ).ready(function() {
 
         });
     });
-    
+    loadSp();
+
+    function loadSp(isEvenChange) {
+        var  tongTiensp = 0;
+
+        $('.giaTien').each(function () {
+            var giatien = $(this).text();
+            var format=  parseFloat(giatien).toFixed(3).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.").toString();
+            // alert(parseFloat(tongTiensp + parseFloat(format)))
+            var tong = tongTiensp + parseFloat(format);
+            tongTiensp = tong;
+
+            if(!isEvenChange )
+            {
+
+                $(this).html(format);
+            }
+
+
+
+            var formatTongTien =  tong.toFixed(3).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.").toString()
+
+            $('#tongtien').html(formatTongTien + "");
+
+        })
+
+
+    }
+
+
+    $('.soluong').change(function () {
+
+
+        var soluong = $(this).val();
+        var giatien = $(this).closest('tr').find('.giaTien').attr('data-giatien');
+        var  tongTien = soluong * giatien;
+
+        var format = tongTien.toFixed(3).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+
+        $(this).closest('tr').find('.giaTien').html(format);
+        loadSp(true);
+
+
+    })
     
     
 });
